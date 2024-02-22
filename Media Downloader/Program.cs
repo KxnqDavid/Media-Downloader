a92409adc3648316a69b2bd3bf9314fbc316aa13
KxnqDavid <do300@DESKTOP-T7S2B1U>
2024-02-21 18:07:19 -0600

    Add project files.

---
+++ Media Downloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Downloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
